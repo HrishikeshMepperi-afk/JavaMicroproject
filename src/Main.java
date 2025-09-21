48e836bc7ffbef14a136a962d1c01840b623cd40
hrishikesh mepperi <hmepperi@gmail.com>
2025-09-21 09:21:01 +0530

    On branch gui  Changes to be committed: 	modified:   src/Main.java
     Untracked files:
    	.idea/
    	LoanSanction.iml

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
+
+    public static void createAndShowGui(){
+        JFrame frame = new JFrame("LoanSanction");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        frame.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                createAndShowGui();
+            }
+        });
     }
 }
